d72781b958283708a8b4b7c1bb9fab9598fbb506
HYUNSU RO <150118274+JUYAMONSTER@users.noreply.github.com>
2024-08-20 15:40:25 +0900

    Update ProjectPage.js

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -12,7 +12,7 @@ function ProjectPage() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   useEffect(() => {
-    axios.get(`${API_URL}/api/posts`)
+    axios.get(`${API_URL}/api/board`)
       .then(response => {
         setPosts(response.data);
       })
